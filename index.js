6eabf9e62481641edcf4a87d2965a448b1321fb1
Fabricio Paoloni <paolonifabricio@gmail.com>
2025-02-07 21:46:17 -0300

    uibuild

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ morgan.token('request-body', (req, res) => {
 app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms --- :request-body'))
-
+app.use(express.static('dist'))
 
 let persons = [
     {
