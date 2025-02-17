0df04352c8aec5a22420e2d6f08f4d0f7f0ba87a
Fabricio Paoloni <paolonifabricio@gmail.com>
2025-02-16 21:07:56 -0300

    3.16: Phonebook database, step 4 - COMPLETED

--- index.js
+++ index.js
@@ -68,24 +68,20 @@ app.get('/info', (request, response) => {
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
             response.json(person)
         })
-        .catch(error => response.status(400).send("Error ocurred while getting the person. Error:", error.message))
-    // let person = persons.find(p => p.id === id)
-    // person ?
-    // response.json(person) :
-    // response.status(404).send('Id not found in the database')
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
    Person.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
         })
-        .catch(error => response.status(400).send(`Error ocurred whil deleting: ${error.message}`))
+        .catch(error => next(error))
 })
 
 // const getRandomID = () => {
@@ -120,6 +116,16 @@ app.post('/api/persons', (request, response) => {
 
 })
 
+//error handler middleware
+const errorHandler = (error, request, response, next) => {
+    if(error.name === 'CastError'){
+        response.status(400).send(`Error: Malformatted id`)
+    }
+
+    next(error)
+}
+//use error handler middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
